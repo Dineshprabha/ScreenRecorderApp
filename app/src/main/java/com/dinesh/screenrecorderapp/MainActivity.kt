b0f040d3215417b66bd645d93b3aace6b07ef531
Dinesh N <dinesh.prabha1205@gmail.com>
2025-01-22 12:15:00 +0530

    Initial commit

---
+++ app/src/main/java/com/dinesh/screenrecorderapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.dinesh.screenrecorderapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.dinesh.screenrecorderapp.ui.theme.ScreenRecorderAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ScreenRecorderAppTheme {
+
+            }
+        }
+    }
+}
