548db1efea335d76da9a1d62f97123ffa6c487a6
Dinesh N <dinesh.prabha1205@gmail.com>
2025-01-24 12:53:13 +0530

    Add annotation

--- app/src/main/java/com/dinesh/screenrecorderapp/MainActivity.kt
+++ app/src/main/java/com/dinesh/screenrecorderapp/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.dinesh.screenrecorderapp
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import com.dinesh.screenrecorderapp.ui.ScreenRecorderApp
 import com.dinesh.screenrecorderapp.ui.theme.ScreenRecorderAppTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
